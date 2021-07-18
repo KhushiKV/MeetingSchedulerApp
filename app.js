d3c24114b48ae005f81c08d0f31d53ab3bb9137e
K_Sneha <snehajyothi2302@gmail.com>
2021-07-18 14:54:31 +0530

    added database

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/meetingDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
 app.set("view engine", "ejs");
 
-const meetings=[];
+const meetingSchema={
+  title:String,
+  details: String,
+  eventId:String
+};
+
+const Schedule = mongoose.model("Schedule", meetingSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -20,17 +29,40 @@ app.get("/create", (req,res)=>{
 });
 
 app.get("/schedule", (req, res)=>{
-  res.render("schedule", {newMeetings: meetings});
+  // res.render("schedule", {newMeetings: meetings});
+  Schedule.find({}, (err, meetings)=>{
+    res.render("schedule", {
+      newMeetings: meetings
+    });
+  });
 });
 
 app.post("/create", (req, res)=>{
-  let meeting = {
+  let meeting = new Schedule({
     title : req.body.scheduleTitle,
     details : req.body.scheduleTime,
-    id : req.body.scheduleId
-  }
-  meetings.push(meeting);
-  res.redirect("/schedule");
+    eventId : req.body.scheduleId
+  });
+  meeting.save((err)=>{
+    if(!err){
+      res.redirect("/schedule");
+    }
+  });
+  // meetings.push(meeting);
+  // res.redirect("/schedule");
+});
+
+app.post("/delete", (req, res)=>{
+  const checkedItemId = (req.body.checkbox);
+  Schedule.deleteOne({_id:checkedItemId}, (err)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("success");
+      res.redirect("/schedule");
+    }
+  });
 });
 
 app.listen(3000, ()=>{
