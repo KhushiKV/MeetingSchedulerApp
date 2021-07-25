4e539a37d112a267358e2d5c5434434ed78df097
KhushiKV <78869960+KhushiKV@users.noreply.github.com>
2021-07-26 00:08:50 +0530

    added authentication

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from "express";
 import  ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -11,9 +13,13 @@ const __dirname = path.resolve();
 
 const app =express();
 
-mongoose.connect("mongodb+srv://admin-sneha:test123@cluster0.q2lcn.mongodb.net/meetingDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-sneha:test123@cluster0.q2lcn.mongodb.net/meetingDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const meetingSchema={
   title:String,
@@ -21,10 +27,16 @@ const meetingSchema={
   eventId:String
 };
 
-const Schedule = mongoose.model("Schedule", meetingSchema);
+const userSchema = {
+  fname:String,
+  lname:String,
+  email:String,
+  password:String,
+  googleId:String
+};
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const Schedule = mongoose.model("Schedule", meetingSchema);
+const User = new mongoose.model("User",userSchema);
 
 app.get("/",(req,res)=>{
 
@@ -55,6 +67,56 @@ app.get("/myschedule", (req, res)=>{
     });
   });
 });
+
+app.get("/logout", (req,res) => {
+   res.sendFile(__dirname+"/public/loginregister.html");
+})
+
+
+app.post("/signup",(req,res)=>{
+  if(req.body.password === req.body.confirmpassword)
+  {
+    bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+      const newUser = new User({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        password: hash
+      });
+      newUser.save(function(err){
+        if(err) console.log(err);
+        else {
+               res.render("schedulehome");
+             }
+      });
+    });
+  }else {
+    res.redirect("/loginregister");
+  }
+
+});
+
+app.post("/login", (req,res)=>{
+
+  const enteredEmail = req.body.email;
+  const enteredPassword= req.body.password;
+
+  User.findOne({email:enteredEmail}, function(err,foundUser){
+    if(!err){
+      if(foundUser){
+        bcrypt.compare(enteredPassword, foundUser.password, function(err, result){
+          if(!err){
+            if(result === true){
+              res.render("schedulehome");
+            }else{console.log(err);res.redirect("/loginregister");}
+          }
+        })
+      }
+    }else{console.log(err);}
+  })
+})
+
+
 
 app.post("/create", (req, res)=>{
   let meeting = new Schedule({
