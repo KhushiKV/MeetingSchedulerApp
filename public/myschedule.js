843647b58e463a9110352b6598592f38521ba171
Mir-Razee <85749685+Mir-Razee@users.noreply.github.com>
2021-08-03 18:02:24 +0530

    Added calendar and inputs

---
+++ public/myschedule.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      initialDate: '2021-07-07',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+    });
+  
+    calendar.render();
+  });
