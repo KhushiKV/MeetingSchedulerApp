93ce89e9affd55c3cfa96bbcef82e08a1699f3b1
Mir Razee <mirrazee0@gmail.com>
2021-08-04 21:25:43 +0530

    added initialdate

--- public/myschedule.js
+++ public/myschedule.js
@@ -2,9 +2,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
   
+    var today=new Date();
+    if(today.getMonth()<'10'){
+      if(today.getDate()<'10'){
+        var date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-0'+today.getDate();
+      }
+      else{
+        var date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-'+today.getDate();
+      }
+    }
+    else{
+      if(today.getDate()<'10'){
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-0'+today.getDate();
+      }
+      else{
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      }
+    }
     var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
-      initialDate: '2021-07-07',
+      initialDate: date,
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
